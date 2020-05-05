e2c7c32aaaecabfbe470182c50c1a60f148d8941
coledworld <64798128+coledworld@users.noreply.github.com>
2020-05-05 15:10:58 -0400

    Begin implementation of Uno command
    Should: accept new game with !uno [value] (value will be bot's currency, right now just enter anything)
                 allow users to join by reacting with a check, and begin game when commander reacts with an X

---
+++ src/main/java/com/malt/aster/commands/UnoCommand.java
@@ -0,0 +1,72 @@
+package com.malt.aster.commands;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.pagination.ReactionPaginationAction;
+import net.dv8tion.jda.api.utils.Procedure;
+
+import java.util.List;
+
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+
+/**
+ * Sets the name of the bot in the server it is called in
+ */
+public class UnoCommand extends Command implements AdminCommand {
+	
+	private EventWaiter waiter;
+
+    public UnoCommand(EventWaiter waiter) {
+        super("uno");
+        addAlias("localname");
+        
+        this.waiter = waiter;
+    }
+
+    @Override
+    public void execute(GuildMessageReceivedEvent event, List<String> params) {
+        if(params.size() == 0)
+            event.getChannel().sendMessage("Need to set gambling value... WIP").queue();
+        else {
+        	
+        	User commander = event.getAuthor();
+        	
+        	//Send the initial message allowing users to join and the commander to start the game
+        	//using reactions
+        	Message startUno = event.getChannel().sendMessage("UNO started! React to this message"
+        			+ " with a CHECK to join, the game will begin when @" + 
+        			commander.getName() + " reacts with an 'X'").complete();
+        	startUno.addReaction("U+2705").queue(); //Check
+        	startUno.addReaction("U+274E").queue(); //X
+        	
+        	this.waiter.waitForEvent(MessageReactionAddEvent.class, e -> e.getUser().equals(commander) && e.getReactionEmote().getAsCodepoints().equals("U+274E"), e ->
+        	{
+        		event.getChannel().sendMessage("jsjsjasjasjjsak").queue();
+        		ReactionPaginationAction players = startUno.retrieveReactionUsers("U+2705");
+        		players.forEach((user) ->
+        			event.getChannel().sendMessage(user.getName()).queue()
+        		);
+        		
+        	});
+        	
+        	
+        	
+        	//Generate a list of all the players (those who have reacted with a check)
+        	
+        	
+        	
+        	
+        	
+        	
+        }
+    }
+
+    
+    @Override
+    public String getDescription() {
+        return "Starts and runs a game of UNO";
+    }
+}
